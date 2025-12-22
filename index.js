949ad101a9cd38a18ba17ebf96caf5b9028b56de
astra-the-boop <astra.the.boop@icloud.com>
2025-12-22 22:12:14 +0700

    grrrrr

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+function updatePreview(){
+    const preview = document.getElementById('preview-box');
+    const fontSelect = document.getElementById('font-select');
+
+    switch(fontSelect.value) {
+        case "times":
+            preview.style.fontFamily = `"Times New Roman", times, serif`;
+            break;
+        case "jetbrains":
+            preview.style.fontFamily = `"JetBrains Mono", monospace`;
+            break;
+        case "phantom":
+            preview.style.fontFamily = `"Phantom Sans", sans-serif`;
+            break;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const fontSelect = document.getElementById('font-select');
 
     fontSelect.addEventListener('change', () => {
+        updatePreview();
         switch(fontSelect.value) {
             case "times":
                 fontSelect.style.fontFamily = `"Times New Roman", times, serif`;
                 break;
             case "jetbrains":
-                console.log("dfkldfgklgdfjlk");
+                fontSelect.style.fontFamily = `"JetBrains Mono", monospace`;
                 break;
             case "phantom":
+                fontSelect.style.fontFamily = `"Phantom Sans", sans-serif`;
                 break;
         }
     })
