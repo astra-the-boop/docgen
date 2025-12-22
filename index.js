85afa6aaddbbd0dc66a32bba975d4c31b8937819
astra-the-boop <astra.the.boop@icloud.com>
2025-12-22 17:39:38 +0700

    implodes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+document.addEventListener('DOMContentLoaded', () => {
+    const fontSelect = document.getElementById('font-select');
+
+    fontSelect.addEventListener('change', () => {
+        switch(fontSelect.value) {
+            case "times":
+                fontSelect.style.fontFamily = `"Times New Roman", times, serif`;
+                break;
+            case "jetbrains":
+                console.log("dfkldfgklgdfjlk");
+                break;
+            case "phantom":
+                break;
+        }
+    })
+})
